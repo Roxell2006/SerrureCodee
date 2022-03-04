495cef7271012a81072737852f5b7657d8577815
Roxell2006 <romualdhansen2006@gmail.com>
2022-03-04 21:27:47 +0100

    Ajout de la fonction VerifieCode

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,30 +7,37 @@ namespace SerrureCodee
 {
     public partial class MainPage : ContentPage
     {
+        Random rand = new Random();
+        int code = 0;
+        int chance = 0;
+        Label[,] label = new Label[4, 5];
 
         public MainPage()
         {
             InitializeComponent();
 
-            Label[,] label = new Label[4, 5];
+            // tire un code aléatoire entre 0 et 9999
+            code = rand.Next(10000);
+            
             int offset = 0; // Permet de sauter la deuxième rangée (row)
 
-            // Création de la Grid (dessin du plateau de jeu)
+            // Création de la Grid (dessin du plateau de jeu et création des labels)
             for (int r = 0; r < 5; r++)
             {
                 for (int c = 0; c < 4; c++)
                 {
-                    // BoxView
-                    BoxView boxView = new BoxView { Color = Color.FromRgb(48, 48, 48) };
                     if (r > 0)
                         offset = 1;
+                    
+                    // BoxView
+                    BoxView boxView = new BoxView { Color = Color.FromRgb(48, 48, 48) };
                     GameGrid.SetRow(boxView, r + offset);
                     GameGrid.SetColumn(boxView, c);
-
+ 
                     // Label
                     label[c, r] = new Label
                     {
-                        Text = c.ToString(),
+                        Text = "",
                         FontSize = 48,
                         HorizontalOptions = LayoutOptions.Center,
                         VerticalOptions = LayoutOptions.Center
@@ -46,12 +53,6 @@ namespace SerrureCodee
 
             //mise à jour de la grid dans la ContentPage;
             Content = GameGrid;
-
-            //Change le text du Label 1 (test)
-            label[1, 0].Text = "R";
-
-            //Ajoute un cercle sur le label 2 (test)
-            AfficheCercle(0, 2);
         }
 
         void AfficheCercle(int col, int row)
@@ -78,26 +79,49 @@ namespace SerrureCodee
         void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
             // Vérifie si l'utilisateur entre bien un nombre
-            bool isDigit = true;
             try
             {
                 if (e.NewTextValue != null)
                 {
-                    int value = int.Parse(e.NewTextValue);
+                    string value = e.NewTextValue;
+                    int test = int.Parse(value);
+                    // Affiche la proposition
+                    label[value.Length -1, chance + 1].Text = value[value.Length-1].ToString();
                     // vérifie si le code contient 4 chiffres
                     if (EntryCode.Text.Length == 4)
                     {
-                        DisplayAlert("Alert", "Le code est: " + EntryCode.Text, "X");
+                        VerifieCode(code.ToString("D4"), EntryCode.Text);
                         EntryCode.Text = null;
                     }
                 }
             }
             catch
             {
-                isDigit = false;
-            }
-            if (!isDigit)
+                // si l'utilisateur a tapé autre chose qu'un chiffre on annule le dernier changement
                 EntryCode.Text = e.OldTextValue;
+            }
+        }
+
+        void VerifieCode(string code, string proposition)
+        {
+            for(int i = 0; i< 4; i++)
+            {
+                if (code.Contains(proposition[i]))
+                {
+                    if (code[i] == proposition[i])
+                        label[i, 0].Text = code[i].ToString();          // Dévoile les bons chiffres
+                    else
+                        AfficheCercle(i, chance);                       // Entoure le chiffre mal placé
+                }
+            }
+            if(code == proposition)
+                DisplayAlert("Vous avez Gagné", "Le code était: " + code, "OK");
+            else
+            {
+                chance++;
+                if (chance == 4)
+                    DisplayAlert("Vous avez perdu", "Le code était: " + code, "OK");
+            }
         }
     }
 }
